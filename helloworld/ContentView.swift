047b222def50016532fc51936976eb5b0b25949b
d <w>
2022-08-14 20:34:07 -0500

    Initial Commit

---
+++ helloworld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  helloworld
+//
+//  Created by Dannis Whitfield on 8/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Dannis!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ helloworld/helloworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloworldApp.swift
+//  helloworld
+//
+//  Created by Dannis Whitfield on 8/14/22.
+//
+
+import SwiftUI
+
+@main
+struct helloworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
